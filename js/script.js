3b4cfa2c3519c886bd49b34715bdfaac2b49b361
radarbandung <ahmadmuzakisyafii@gmail.com>
2022-06-14 20:51:42 +0700

    Finishing Display

--- js/script.js
+++ js/script.js
@@ -1,70 +1,40 @@
-const game = () => {
- 
-    // Function to
-    const playGame = () => {
-        const tombolBatu = document.querySelector('.batu');
-        const tombolKertas = document.querySelector('.kertas');
-        const tombolGunting = document.querySelector('.gunting');
-        const playerOptions = [tombolBatu,tombolKertas,tombolGunting];
-        const computerOptions = ['.batu','.kertas','.gunting'];
-         
-        // Function to start playing game
-        playerOptions.forEach(option => {
-            option.addEventListener('click',function(){
-
-                const choiceNumber = Math.floor(Math.random()*3);
-                const computerChoice = computerOptions[choiceNumber];
- 
-                // Function to check who wins
-                winner(this.innerText,computerChoice)
-            })
-        })
-         
+var userChoice = prompt("Do you choose rock, paper or scissors?");
+var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+	computerChoice = "rock";
+} else if(computerChoice <= 0.67) {
+	computerChoice = "paper";
+} else {
+	computerChoice = "scissors";
+}
+var compare=function(choice1,choice2){
+    if(choice1===choice2){
+        return "The result is a tie!";
     }
- 
-    // Function to decide winner
-    const winner = (player,computer) => {
-        const result = document.querySelector('.result');
-        player = player.toLowerCase();
-        computer = computer.toLowerCase();
-        if(player === computer){
-            result.textContent = 'Seri'
+    else if(choice1==="rock"){
+        if(choice2==="scissors"){
+            return "rock wins"
         }
-        else if(player == 'rock'){
-            if(computer == 'paper'){
-                result.textContent = 'Computer Menang';
-            }else{
-                result.textContent = 'Player Menang';
-            }
-        }
-        else if(player == 'scissors'){
-            if(computer == 'rock'){
-                result.textContent = 'Computer Menang';
-            }else{
-                result.textContent = 'Player Menang';
-            }
-        }
-        else if(player == 'paper'){
-            if(computer == 'scissors'){
-                result.textContent = 'Computer Menang';
-            }else{
-                result.textContent = 'Player Menang';
-            }
+        else if(choice2==="paper"){
+            return "paper wins";
         }
     }
- 
-        const reloadBtn = document.querySelector('.reload');
-        
-        reloadBtn.innerText = 'Restart';
-        reloadBtn.style.display = 'flex'
-        reloadBtn.addEventListener('click',() => {
-            window.location.reload();
-        })
+    else if(choice1==="paper"){
+        if(choice2==="scissors"){
+            return "scissors win"
+        }
+        else if(choice2==="rock"){
+            return "paper wins";
+        }
     }
- 
- 
-    // Calling playGame function inside game
-    playGame();
- 
-// Calling the game function
-game();
+    else if(choice1==="scissors"){
+        if(choice2==="paper"){
+            return "scissors win"
+        }
+        else if(choice2==="rock"){
+            return "rock wins";
+        }
+    }
+    
+}
+console.log(compare(userChoice,computerChoice));
