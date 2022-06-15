f9c0188db6cbb5aa36eeffeb21720a4e3f124d2a
radarbandung <ahmadmuzakisyafii@gmail.com>
2022-06-15 09:13:38 +0700

    Finishing Function

--- js/script.js
+++ js/script.js
@@ -1,40 +1,62 @@
-var userChoice = prompt("Do you choose rock, paper or scissors?");
-var computerChoice = Math.random();
-if (computerChoice < 0.34) {
-	computerChoice = "rock";
-} else if(computerChoice <= 0.67) {
-	computerChoice = "paper";
-} else {
-	computerChoice = "scissors";
+const result_div = document.querySelector(".result");
+const pilihBatu = document.getElementById("batu");
+const pilihKertas = document.getElementById("kertas");
+const pilihGunting = document.getElementById("gunting");
+
+// Computer
+function getComputerChoice() {
+  const choices = ["batu", "kertas", "gunting"];
+  const randomNumber = Math.floor(Math.random() * 3);
+  return choices[randomNumber];
 }
-var compare=function(choice1,choice2){
-    if(choice1===choice2){
-        return "The result is a tie!";
-    }
-    else if(choice1==="rock"){
-        if(choice2==="scissors"){
-            return "rock wins"
-        }
-        else if(choice2==="paper"){
-            return "paper wins";
-        }
-    }
-    else if(choice1==="paper"){
-        if(choice2==="scissors"){
-            return "scissors win"
-        }
-        else if(choice2==="rock"){
-            return "paper wins";
-        }
-    }
-    else if(choice1==="scissors"){
-        if(choice2==="paper"){
-            return "scissors win"
-        }
-        else if(choice2==="rock"){
-            return "rock wins";
-        }
-    }
-    
+
+// Menang
+function win() {
+  result_div.innerHTML = `Player Win!`;
 }
-console.log(compare(userChoice,computerChoice));
+
+// Kalah
+function loses() {
+  result_div.innerHTML = `Com Win!`;
+}
+
+// Seri
+function draw() {
+  result_div.innerHTML = `Draw!`;
+}
+
+// Game
+function game(userChoice) {
+  const computerChoice = getComputerChoice();
+
+  switch (userChoice + computerChoice) {
+    case "kertasbatu":
+    case "batugunting":
+    case "guntingkertas":
+      win(userChoice, computerChoice);
+      break;
+    case "batukertas":
+    case "guntingbatu":
+    case "kertasgunting":
+      loses(userChoice, computerChoice);
+      break;
+    case "batubatu":
+    case "guntinggunting":
+    case "kertaskertas":
+      draw(userChoice, computerChoice);
+      break;
+  }
+}
+
+// User
+function main() {
+    pilihBatu.addEventListener("click", () => game("batu"));
+    pilihKertas.addEventListener("click", () => game("kertas"));
+    pilihGunting.addEventListener("click", () => game("gunting"));
+}
+
+// Reset
+document.getElementById("resetbutton").addEventListener("click", function(){
+        alert("Reset The Game!");
+        main();
+});
